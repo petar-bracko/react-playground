ec37cdd3f49a5d1626d9ac5eab9ba5dd2ad22477
Petar Bracko <petar.bracko@gmail.com>
2023-02-03 20:01:08 +0100

    useReducer state handling pattern

--- src/pages/UseStateHell.tsx
+++ src/pages/UseStateHell.tsx
@@ -1,3 +1,41 @@
+import { useReducer } from "react";
+
+type TInfo = {
+  name: string;
+  foo: string;
+};
+
+const initInfo = { name: "petar", foo: "bar" };
+
 export const UseStateHell = () => {
-  return <div>UseStateHell</div>;
+  const [info, setInfo] = useReducer((current: TInfo, next: any): TInfo => {
+    const result: TInfo = { ...current, ...next };
+
+    if (next.name.length > 10) {
+      console.log("here");
+      next.name = current.name;
+    } else {
+      console.log("skipping");
+    }
+
+    return result;
+  }, initInfo);
+
+  return (
+    <>
+      <h2>UseStateHell</h2>
+      <div>
+        <pre>{JSON.stringify(info, null, 2)}</pre>
+      </div>
+      <input
+        value={info.name}
+        onChange={(e) => setInfo({ name: e.target.value })}
+        // maxLength={10}
+      />
+      <input
+        value={info.foo}
+        onChange={(e) => setInfo({ foo: e.target.value })}
+      />
+    </>
+  );
 };
